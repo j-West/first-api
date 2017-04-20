7b76a08666cb82a8c139a2161ec21de3ba5c2c0c
j-west <westja89@gmail.com>
2017-04-20 11:31:35 -0500

    Add method to get all directors

--- models/director.js
+++ models/director.js
@@ -6,6 +6,18 @@ require('./show')
 const Director = bookshelf.Model.extend({ 
   tableName: 'directors',
   shows: function() { return this.belongsToMany('Show').through('Show_Director')}
+}, {
+  getAll: function() {
+    console.log("Get all called from Director Model")
+    return this.forge()
+    .fetchAll()
+    .then( (rows) => {
+      return rows
+    })
+    .catch( (error) => {
+      return error
+    })
+  }
 })
 
 module.exports = bookshelf.model('Director', Director)
