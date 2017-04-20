e1a9320d22bb935130c937102a7127e3572229ea
j-west <westja89@gmail.com>
2017-04-20 11:35:17 -0500

    comment new table migrations

--- db/migrations/20170417202008_shows.js
+++ db/migrations/20170417202008_shows.js
@@ -13,22 +13,22 @@ exports.up = (knex, Promise) => {
     table.timestamp('dateAdded').notNullable().defaultTo(knex.fn.now())
     table.integer('show_id').unsigned().references('shows.id')
   })
-  .createTable('directors', (table) => { 
-    table.increments()
-    table.string('name').notNullable().unique()
-    table.string('gender').notNullable()
-    table.integer('birth_year')
-    table.string('twitter_handle')
-  })
-  .createTable('shows_directors', (table) => {
-    table.increments()
-    table.integer('director_id').unsigned().references('directors.id')
-    table.integer('show_id').unsigned().references('shows.id')
-  })
+  // .createTable('directors', (table) => { 
+  //   table.increments()
+  //   table.string('name').notNullable().unique()
+  //   table.string('gender').notNullable()
+  //   table.integer('birth_year')
+  //   table.string('twitter_handle')
+  // })
+  // .createTable('shows_directors', (table) => {
+  //   table.increments()
+  //   table.integer('director_id').unsigned().references('directors.id')
+  //   table.integer('show_id').unsigned().references('shows.id')
+  // })
 }
 
 exports.down = (knex, Promise) => knex.schema
-                                  .dropTable('shows_directors')
+                                  // .dropTable('shows_directors')
                                   .dropTable('favorites')
                                   .dropTable('shows')
-                                  .dropTable('directors')
+                                  // .dropTable('directors')
