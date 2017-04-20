26a7af81849196b032e47bd75fca1d0a06c553fe
j-west <westja89@gmail.com>
2017-04-20 11:31:58 -0500

    Add two routes, one to get all directors, the second to get a directors shows

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 const { Router } = require('express')
 const router = Router()
 
-const { getShows, getShow, getShowFaves, addShow, deleteShow, getShowDirectors } = require('../controllers/showCtrl')
+const { getShows, getShow, getShowFaves, addShow, deleteShow, getShowDirectors, getDirectors, getDirectorShows } = require('../controllers/showCtrl')
 
 router.get('/shows', getShows)
 router.post('/shows/new', addShow)
@@ -11,5 +11,8 @@ router.delete('/shows/:id', deleteShow)
 router.get('/shows/favorites', getShowFaves)
 router.get('/shows/directors', getShowDirectors)
 router.get('/shows/:id', getShow)
+
+router.get('/directors', getDirectors)
+router.get('/directors/shows', getDirectorShows)
 
 module.exports = router
