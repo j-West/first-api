b3005483d416731d4860a170546e8ec5bef43c22
j-west <westja89@gmail.com>
2017-04-20 09:30:22 -0500

    refactor to arrow functions, add directors table and shows_directors pivot table

--- db/migrations/20170417202008_shows.js
+++ db/migrations/20170417202008_shows.js
@@ -1,17 +1,33 @@
 
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('shows', function(table) {
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('shows', (table) => {
     table.increments()
     table.string('name').notNullable().unique()
     table.string('channel').notNullable()
     table.string('genre').notNullable()
     table.boolean('inProduction').notNullable()
   })
-  .createTable('favorites', function(table) {
+  .createTable('favorites', (table) => {
     table.increments()
     table.timestamp('dateAdded').notNullable().defaultTo(knex.fn.now())
     table.integer('show_id').unsigned().references('shows.id')
   })
+  .createTable('directors', (table) => { 
+    table.increments()
+    table.string('name').notNullable().unique()
+    table.string('gender').notNullable()
+    table.integer('birth_year')
+    table.string('twitter_handle')
+  })
+  createTable('shows_directors', (table) => {
+    table.increments()
+    table.integer('directo_id').unsigned().references('directors.id')
+    table.integer('show_id').unsigned().references('shows.id')
+  })
 }
 
-exports.down = (knex, Promise) => knex.schema.dropTable('favorites').dropTable('shows')
+exports.down = (knex, Promise) => knex.schema
+                                  .dropTable('shows_directors')
+                                  .dropTable('favorites')
+                                  .dropTable('shows')
+                                  .dropTable('directors')
